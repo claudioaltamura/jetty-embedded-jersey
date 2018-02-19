db9b857de04e8796297502181ed27023cc98161a
claudio <claudio@claudioaltamura.de>
2018-02-19 11:03:13 +0100

    changed methods

--- src/main/java/de/claudioaltamura/jetty/jersey/superheroes/v1/HeroResource.java
+++ src/main/java/de/claudioaltamura/jetty/jersey/superheroes/v1/HeroResource.java
@@ -2,10 +2,10 @@ package de.claudioaltamura.jetty.jersey.superheroes.v1;
 
 import java.util.Collection;
 
-import javax.annotation.PostConstruct;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -23,57 +23,73 @@ public class HeroResource {
 
 	private static final Logger LOG = Logger.getLogger(HeroResource.class);
 
-	protected HeroService heroServivce;
+	protected HeroService heroService;
 
-	//TODO Replace with CDI
+	// TODO Replace with CDI
 	public HeroResource() {
-		heroServivce = new HeroService();
+		heroService = new HeroService();
 	}
 
-	@PostConstruct
-	public void init() {
-		LOG.debug("init");
-	}
-
-	//TODO get example with filtering, sorting, searching
-	///filtering tickets?state=open
-	//sortring GET /tickets?sort=created_at,priority
-	//searching GET /employees?query=Paul
-	//HINT: Query Parameter example
-	//pagination /employees?offset=30&limit=15 # returns the employees 30 to 45
-	
-	//Updates & creation should return a resource representation
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Collection<Hero> findall() {
-		return heroServivce.findAll();
+		//TODO Change return type to Response
+		LOG.info("find all persons");
+		return heroService.findAll();
 	}
 
 	@GET
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	//HINT: Path Parameter example
-	public Hero find(@PathParam("id") int id) {
-		return heroServivce.findById(id);
+	public Response find(@PathParam("id") int id) {
+		LOG.info("find=" + id);
+		Hero hero = heroService.findById(id);
+
+		if (hero == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		} else {
+			return Response.ok().build();
+		}
 	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response create(Hero person) {
-		Integer index = heroServivce.add(person);
-		return Response.status(200).entity(index).build();
+		LOG.info("create=" + person);
+
+		Integer index = heroService.create(person);
+
+		return Response.status(201).entity(index).build();
 	}
 
 	// @PUT
 
+	@PATCH
+	@Path("{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response patch(@PathParam("id") int id, String realname) {
+		LOG.info("patch=" + id);
+
+		if(heroService.exists(id)) {
+			Hero hero2Updated = heroService.findById(id);
+			hero2Updated.setRealname(realname);
+			heroService.update(hero2Updated);
+			return Response.ok().build();
+		} else {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+	}
+
 	@DELETE
 	@Path("{id}")
 	public Response delete(@PathParam("id") int id) {
-		//TODO What should Return HTTP DELETE? 200 or 204
-		LOG.info("delete="+id);
-		heroServivce.deleteById(id);
-		//NO Content
-		return Response.status(204).build();
+		LOG.info("delete=" + id);
+
+		if(heroService.exists(id)) {
+			heroService.delete(id);
+			return Response.status(204).build();
+		} else
+			return Response.status(Response.Status.NOT_FOUND).build();
 	}
 
 }
