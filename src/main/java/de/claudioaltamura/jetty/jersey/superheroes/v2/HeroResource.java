a206e332dc7a8161efe3915ea44f57e0579b47e9
claudio <claudio@claudioaltamura.de>
2018-02-19 11:03:25 +0100

    changed extends v1

--- src/main/java/de/claudioaltamura/jetty/jersey/superheroes/v2/HeroResource.java
+++ src/main/java/de/claudioaltamura/jetty/jersey/superheroes/v2/HeroResource.java
@@ -11,9 +11,10 @@ public class HeroResource extends de.claudioaltamura.jetty.jersey.superheroes.v1
 	//searching GET /employees?query=Paul
 	//HINT: Query Parameter example
 	//pagination /employees?offset=30&limit=15 # returns the employees 30 to 45
-	
 	//Updates & creation should return a resource representation
 
-
-	//TODO neben POST, PUT und DELETE auch PATCH
+	//@QueryParam
+	//@PathParam
+	//@FormParam
+	
 }
