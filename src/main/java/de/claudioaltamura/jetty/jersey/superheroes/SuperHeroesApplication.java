b5566dfb99a743ac1e27366406692f6985c785c7
claudio <claudio@claudioaltamura.de>
2018-03-26 13:49:10 +0200

    added bean validation

--- src/main/java/de/claudioaltamura/jetty/jersey/superheroes/SuperHeroesApplication.java
+++ src/main/java/de/claudioaltamura/jetty/jersey/superheroes/SuperHeroesApplication.java
@@ -2,17 +2,22 @@ package de.claudioaltamura.jetty.jersey.superheroes;
 
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
 
 /**
  * Configures an JAX-RS Application.
+ * 
  * @see https://jersey.github.io/documentation/latest/deployment.html#deployment.servlet.3
  */
-//@ApplicationPath("/superheroes/api")
+// @ApplicationPath("/superheroes/api")
 public class SuperHeroesApplication extends ResourceConfig {
 
 	public SuperHeroesApplication() {
-		super(
-			JacksonFeature.class
-		);
+		super(JacksonFeature.class);
+		property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
+		property(ServerProperties.BV_DISABLE_VALIDATE_ON_EXECUTABLE_OVERRIDE_CHECK, true);
+
+		packages("de.claudioaltamura.jetty.jersey");
+
 	}
 }
