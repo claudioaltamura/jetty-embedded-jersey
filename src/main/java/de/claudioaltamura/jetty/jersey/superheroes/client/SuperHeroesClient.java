758fddb77a308eb9065d0422d9fbf47db9ae87ea
Claudio Altamura <claudio@claudioaltamura.de>
2018-06-15 12:32:36 +0200

    changed client code for validation exception mapping

--- src/main/java/de/claudioaltamura/jetty/jersey/superheroes/client/SuperHeroesClient.java
+++ src/main/java/de/claudioaltamura/jetty/jersey/superheroes/client/SuperHeroesClient.java
@@ -11,16 +11,31 @@ import de.claudioaltamura.jetty.jersey.superheroes.entity.Hero;
 
 public class SuperHeroesClient {
 
-    private static final String URI = "http://localhost:8080/v1";
+    private static final String URI = "http://localhost:8080/v1";;
     
     private Client client = ClientBuilder.newClient();
 
     public Hero getHero(long id) {
-    	return client.target(URI).path("heroes").path(Long.toString(id)).request(MediaType.APPLICATION_JSON).get(Hero.class);
+    	return client
+    			.target(URI)
+    			.path("heroes")
+    			.path(Long.toString(id))
+    			.request(MediaType.APPLICATION_JSON)
+    			.get(Hero.class);
     }
     
-    public long addHero(Hero hero) {
-    	Response response = client.target(URI).path("heroes").request(MediaType.APPLICATION_JSON).post(Entity.entity(hero, MediaType.APPLICATION_JSON));
+    public long addHero(Hero hero) throws Exception {
+    	Response response = client
+    			.target(URI)
+    			.path("heroes")
+    			.request(MediaType.APPLICATION_JSON)
+    			.post(Entity.entity(hero, MediaType.APPLICATION_JSON));
+    	
+    	if(response != null && response.getStatus() >= 400)
+    	{
+    		throw new Exception(response.readEntity(String.class));
+    	}
+    	
     	return response.readEntity(Long.class);
     }
         
@@ -42,11 +57,16 @@ public class SuperHeroesClient {
 		//Put
 		Hero newHero = new Hero();
 		newHero.setCity("Hamburg");
-		newHero.setName("HH");
 		newHero.setRealname("Ham Burg");
-		long newId = superHeroesClient.addHero(newHero);
-		newHero.setId(newId);
-		System.out.println(newHero);
+		long newId;
+		try {
+			newId = superHeroesClient.addHero(newHero);
+			newHero.setId(newId);
+			System.out.println(newHero);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 	}
 	
 }
